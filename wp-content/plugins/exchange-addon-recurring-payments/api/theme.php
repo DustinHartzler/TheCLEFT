b3650d0ae172b5384e7df97fe8ffad740372b8de
Dustin Hartzler <Dustin@HartzlerDM.com>
2015-07-25 14:38:14 -0400

    Removed iThemes Exchange - Recurring Payments Add-on

--- wp-content/plugins/exchange-addon-recurring-payments/api/theme.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Loads theme APIs for iThemes Exchange - Recurring Payments Add-on
- *
- * @package exchange-addon-recurring-payments
- * @since 1.0.0
-*/
-include( 'theme/recurring-payments.php' );
--- wp-content/plugins/exchange-addon-recurring-payments/lib/product-features/load.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Includes all of our recurring payments product features
- * @since 1.0.0
- * @package exchange-addon-recurring-payments
-*/
-
-// Product Feature: Recurring Payments
-require( 'class.recurring-payments.php' );
