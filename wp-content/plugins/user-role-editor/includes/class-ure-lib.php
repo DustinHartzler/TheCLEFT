59c2f6695dac67476086f74f5bd60a040183598b
Dustin Hartzler <Dustin@HartzlerDM.com>
2016-01-18 21:14:17 -0500

    Removed plugins

--- wp-content/plugins/user-role-editor/includes/class-ure-lib.php
+++ wp-content/plugins/user-role-editor/includes/class-ure-lib.php
@@ -35,10 +35,7 @@ class Ure_Lib extends Garvs_WP_Lib {
 	protected $capability_remove_html = '';
 	protected $advert = null;
  protected $role_additional_options = null;
-<<<<<<< HEAD
-=======
  protected $bbpress = null; // reference to the URE_bbPress class instance
->>>>>>> origin/master
  
  // when allow_edit_users_to_not_super_admin option is turned ON, we set this property to true 
  // when we raise single site admin permissions up to the superadmin for the 'Add new user' new-user.php page
@@ -58,11 +55,8 @@ class Ure_Lib extends Garvs_WP_Lib {
                                            
         parent::__construct($options_id); 
         $this->debug = defined('URE_DEBUG') && (URE_DEBUG==1 || URE_DEBUG==true);
-<<<<<<< HEAD
-=======
  
         $this->bbpress = URE_bbPress::get_instance($this);
->>>>>>> origin/master
         
         $this->upgrade();
     }
@@ -770,33 +764,8 @@ if ($this->multisite && !is_network_admin()) {
             $wp_roles = new WP_Roles();
         }                
 
-<<<<<<< HEAD
-        if (function_exists('bbp_filter_blog_editable_roles')) {  // bbPress plugin is active
-            $this->roles = bbp_filter_blog_editable_roles($wp_roles->roles);  // exclude bbPress roles	
-            $bbp_full_caps = bbp_get_caps_for_role(bbp_get_keymaster_role());
-            // exclude capabilities automatically added by bbPress bbp_dynamic_role_caps() and not bbPress related: read, level_0, all s2Member levels, e.g. access_s2member_level_0, etc.
-            $built_in_wp_caps = $this->get_built_in_wp_caps();
-            $bbp_only_caps = array();
-            foreach ($bbp_full_caps as $bbp_cap => $val) {
-                if (isset($built_in_wp_caps[$bbp_cap]) || substr($bbp_cap, 0, 15) == 'access_s2member') {
-                    continue;
-                }
-                $bbp_only_caps[$bbp_cap] = $val;
-            }
-            // remove bbPress dynamically created capabilities from WordPress persistent roles in order to not save them to database with any role update
-            $cap_removed = false;
-            foreach ($bbp_only_caps as $bbp_cap => $val) {
-                foreach ($this->roles as &$role) {
-                    if (isset($role['capabilities'][$bbp_cap])) {
-                        unset($role['capabilities'][$bbp_cap]);
-                        $cap_removed = true;
-                    }
-                }
-            }            
-=======
         if (!empty($this->bbpress)) {  // bbPress plugin is active
             $this->roles = $this->bbpress->get_roles();
->>>>>>> origin/master
         } else {
             $this->roles = $wp_roles->roles;
         }        
@@ -969,11 +938,8 @@ if ($this->multisite && !is_network_admin()) {
      * @return array 
      */
     public function get_built_in_wp_caps() {
-<<<<<<< HEAD
-=======
         $wp_version = get_bloginfo('version');
         
->>>>>>> origin/master
         $caps = array();
         $caps['switch_themes'] = 1;
         $caps['edit_themes'] = 1;
@@ -1030,15 +996,11 @@ if ($this->multisite && !is_network_admin()) {
         $caps['update_core'] = 1;
         $caps['list_users'] = 1;
         $caps['remove_users'] = 1;
-<<<<<<< HEAD
-        $caps['add_users'] = 1;
-=======
                 
         if (version_compare($wp_version, '4.4', '<')) {
             $caps['add_users'] = 1;  // removed from WP v. 4.4.
         }
         
->>>>>>> origin/master
         $caps['promote_users'] = 1;
         $caps['edit_theme_options'] = 1;
         $caps['delete_themes'] = 1;
@@ -1607,8 +1569,6 @@ if ($this->multisite && !is_network_admin()) {
     
     
     /**
-<<<<<<< HEAD
-=======
      * Add bbPress plugin user capabilities (if available)
      */
     protected function add_bbpress_caps() {
@@ -1626,7 +1586,6 @@ if ($this->multisite && !is_network_admin()) {
         
     
     /**
->>>>>>> origin/master
      * Provide compatibility with plugins and themes which define their custom user capabilities using 
      * 'members_get_capabilities' filter from Members plugin 
      * 
@@ -1755,10 +1714,7 @@ if ($this->multisite && !is_network_admin()) {
         $this->full_capabilities = array();
         $this->add_roles_caps();
         $this->add_gravity_forms_caps();
-<<<<<<< HEAD
-=======
         $this->add_bbpress_caps();
->>>>>>> origin/master
         $this->add_members_caps();
         $this->add_user_caps();
         $this->add_wordpress_caps();
