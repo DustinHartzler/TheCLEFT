2733f569770d2cf39a49ac5677095e0b818585ff
Dustin Hartzler <Dustin@HartzlerDM.com>
2016-12-29 16:28:34 -0500

    Updated Outdated WooCommerce Files

--- wp-content/themes/Cleft_2012/woocommerce/emails/customer-invoice.php
+++ wp-content/themes/Cleft_2012/woocommerce/emails/customer-invoice.php
@@ -2,66 +2,46 @@
 /**
  * Customer invoice email
  *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates/Emails
- * @version     2.2.0
+ * @version     2.5.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+ if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+ }
 
-<?php do_action( 'woocommerce_email_header', $email_heading ); ?>
+ /**
+ * @hooked WC_Emails::email_header() Output the email header
+ */
+ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<?php if ( $order->has_status( 'pending' ) ) : ?>
+ <?php if ( $order->has_status( 'pending' ) ) : ?>
+  <p><?php printf( __( 'An order has been created for you on %s. To pay for this order please use the following link: %s', 'woocommerce' ), get_bloginfo( 'name', 'display' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'pay', 'woocommerce' ) . '</a>' ); ?></p>
+ <?php endif; ?>
 
-	<p><?php printf( __( 'An order has been created for you on %s. To pay for this order please use the following link: %s', 'woocommerce' ), get_bloginfo( 'name' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'pay', 'woocommerce' ) . '</a>' ); ?></p>
+ <?php
 
-<?php endif; ?>
+ /**
+ * @hooked WC_Emails::order_details() Shows the order details table.
+ * @hooked WC_Emails::order_schema_markup() Adds Schema.org markup.
+ * @since 2.5.0
+ */
+ do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
 
-<?php do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text ); ?>
+ /**
+ * @hooked WC_Emails::order_meta() Shows order meta data.
+ */
+ do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
 
-<h2><?php echo __( 'Order:', 'woocommerce' ) . ' ' . $order->get_order_number(); ?> (<?php printf( '<time datetime="%s">%s</time>', date_i18n( 'c', strtotime( $order->order_date ) ), date_i18n( wc_date_format(), strtotime( $order->order_date ) ) ); ?>)</h2>
+ /**
+ * @hooked WC_Emails::customer_details() Shows customer details
+ * @hooked WC_Emails::email_address() Shows email address
+ */
+ do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
 
-<table cellspacing="0" cellpadding="6" style="width: 100%; border: 1px solid #eee;" border="1" bordercolor="#eee">
-	<thead>
-		<tr>
-			<th scope="col" style="text-align:left; border: 1px solid #eee;"><?php _e( 'Product', 'woocommerce' ); ?></th>
-			<th scope="col" style="text-align:left; border: 1px solid #eee;"><?php _e( 'Quantity', 'woocommerce' ); ?></th>
-			<th scope="col" style="text-align:left; border: 1px solid #eee;"><?php _e( 'Price', 'woocommerce' ); ?></th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			switch ( $order->get_status() ) {
-				case "completed" :
-					echo $order->email_order_items_table( $order->is_download_permitted(), false, true );
-				break;
-				case "processing" :
-					echo $order->email_order_items_table( $order->is_download_permitted(), true, true );
-				break;
-				default :
-					echo $order->email_order_items_table( $order->is_download_permitted(), true, false );
-				break;
-			}
-		?>
-	</tbody>
-	<tfoot>
-		<?php
-			if ( $totals = $order->get_order_item_totals() ) {
-				$i = 0;
-				foreach ( $totals as $total ) {
-					$i++;
-					?><tr>
-						<th scope="row" colspan="2" style="text-align:left; border: 1px solid #eee; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['label']; ?></th>
-						<td style="text-align:left; border: 1px solid #eee; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['value']; ?></td>
-					</tr><?php
-				}
-			}
-		?>
-	</tfoot>
-</table>
-
-<?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text ); ?>
-
-<?php do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text ); ?>
-
-<?php do_action( 'woocommerce_email_footer' ); ?>
+ /**
+ * @hooked WC_Emails::email_footer() Output the email footer
+ */
+ do_action( 'woocommerce_email_footer', $email );
